b0ff751a44f8a1740c99ae5c8b26020342b5abe2
SharethonShare <189596117+saka11show@users.noreply.github.com>
2026-02-08 19:08:47 +0900

    pagesから起動できるように修正4

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,7 +1,7 @@
 export function Marquee() {
   return (
     <section className="marquee">
-      <img src="/public/文法スロット.png" style={{width: "100%", height:"100%"}} alt="文法スロット"></img>
+      <img src="/bunpo_slot/文法スロット.png" style={{width: "100%", height:"100%"}} alt="文法スロット"></img>
     </section>
   );
 }
